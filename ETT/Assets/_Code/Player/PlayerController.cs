9d0fb8fbe46a7ea2fabd673d7dfe84626650766d
LeinadDH <daniel_navarrete_05@hotmail.com>
2024-05-07 12:00:13 -0600

    Weapon and crosshair

--- ETT/Assets/_Code/Player/PlayerController.cs
+++ ETT/Assets/_Code/Player/PlayerController.cs
@@ -4,10 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private Animator _animator;
-    [SerializeField]
     private float _speed = 5f;
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     private Vector2 _moveInput;
     private Vector2 _smoothedMoveInput;
     private Vector2 _moveInputSmoothedVelocity;
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -23,6 +25,15 @@ public class PlayerController : MonoBehaviour
         _smoothedMoveInput = Vector2.SmoothDamp(
             _smoothedMoveInput, _moveInput, ref _moveInputSmoothedVelocity, 0.1f);
         _rb.velocity = _smoothedMoveInput * _speed;
+        
+        if(_rb.velocity.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if(_rb.velocity.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 
     private void OnMove(InputValue value)
